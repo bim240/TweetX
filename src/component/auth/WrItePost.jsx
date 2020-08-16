f9b7296bd8dc13fe0d59216e41ec88c6c7483feb
bim240 <bimlendu357@gmail.com>
2020-08-16 20:59:09 +0530

    write post UI

--- src/component/auth/WrItePost.jsx
+++ src/component/auth/WrItePost.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import { NavLink } from "react-router-dom";
-import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowBack, IoMdAddCircleOutline } from "react-icons/io";
 import { AiOutlineCloseCircle } from "react-icons/ai";
+import { GrEmoji } from "react-icons/gr";
+import { TiLocationOutline } from "react-icons/ti";
+import { RiSendPlaneLine } from "react-icons/ri";
+
 const WritePost = (props) => {
+  let post = useRef("");
   return (
     <section className="write_post_container">
       <div className="write_post_subcontainer">
@@ -11,10 +16,40 @@ const WritePost = (props) => {
             <IoIosArrowBack />
           </NavLink>
           <div className="description"> Create New Post</div>
-          <NavLink to="/feed">
+          <NavLink to="/feed" className="btn_close">
             <AiOutlineCloseCircle />
           </NavLink>
         </div>
+        <textarea
+          name="writePost"
+          placeholder="Type here"
+          maxLength="200"
+          className="write_post"></textarea>
+        <div className="input_container">
+          <input
+            type="text"
+            placeholder="Add Caption"
+            className="add_caption"
+          />
+          <GrEmoji className="icon" />
+        </div>
+        <div className="input_container">
+          <input type="text" placeholder="Tag prople" className="tag_prople" />
+          <IoMdAddCircleOutline className="icon" />
+        </div>
+        <div className="input_container">
+          <input
+            type="text"
+            placeholder="Add Location"
+            className="add_location"
+          />
+          <TiLocationOutline className="icon" />
+        </div>
+        <NavLink to="/feed" className="submit_post">
+          {" "}
+          <RiSendPlaneLine className="icon" />
+          Post
+        </NavLink>
       </div>
     </section>
   );
